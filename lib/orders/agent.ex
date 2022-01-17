d95aff14f75ceb9066d72b6a878bf9e5c3a670fb
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-01-17 19:56:47 -0300

    feat: saving and get order data on Agent

---
+++ lib/orders/agent.ex
@@ -0,0 +1,26 @@
+defmodule Exlivery.Orders.Agent do
+  alias Exlivery.Orders.Order
+
+  use Agent
+
+  def start_link(_initial) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Order{} = order) do
+    uuid = UUID.uuid4()
+    Agent.update(__MODULE__, fn x -> update_state(x, order, uuid) end)
+    {:ok, uuid}
+  end
+
+  def get(uuid), do: Agent.get(__MODULE__, fn x -> get_order(x, uuid) end)
+
+  defp update_state(state, %Order{} = order, uuid), do: Map.put(state, uuid, order)
+
+  defp get_order(state, uuid) do
+    case Map.get(state, uuid) do
+      nil -> {:error, "order not found"}
+      order -> {:ok, order}
+    end
+  end
+end
